e8a05dfa63dcc778737a465df12c1d8fd5e11909
EugeneHsiung <Eugene.hsiung@stonybrook.edu>
2023-12-19 09:14:49 +0000

    images

---
+++ db/create_database1.sql
@@ -0,0 +1,4 @@
+create database Eugene;
+use Eugene;
+create table pregnancy (current_year int, related varchar(200), Underlying_cause varchar(200), race_ethnicity varchar(200), Borough varchar(200), Deaths int);
+select * from pregnancy
--- db/populate1.py
+++ db/populate1.py
@@ -19,18 +19,15 @@ conn_string = (
 # Create a database engine
 db_engine = create_engine(conn_string, echo=False)
 
-csv = '/home/eugenehsiung/flask_e2e_project/data/Clean-Pregnancy-Associated_Mortality.csv'
-df = pd.read_csv(csv)
-
+df = pd.read_csv('/home/eugenehsiung/flask_e2e_project/data/Clean-Pregnancy-Associated_Mortality.csv')
 engine = create_engine(conn_string) # Create SQLAlchemy Engine
 df.to_sql('pregnancy', con=engine, if_exists='replace', index=False) # Insert data into Azure SQL Database
+
 
 # create database Eugene;
 # use Eugene;
 # create table pregnancy (current_year int, related varchar(200), Underlying_cause varchar(200), race_ethnicity varchar(200), Borough varchar(200), Deaths int);
 # select * from pregnancy
-
-
 
 
 # resource: https://saturncloud.io/blog/writing-a-pandas-dataframe-to-mysql/ 
